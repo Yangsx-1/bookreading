00b61965a27f7f82ba9621e0ac1ba146cdb4d9e3
yangsx <380129340@qq.com>
2022-11-12 23:29:38 +0800

    add liaoxuefeng java course and Hello.java

---
+++ liaoxuefeng-java-learning/Readme.md
@@ -0,0 +1,1 @@
+This is a record for learning LiaoXueFeng's Java course, the course address is https://www.liaoxuefeng.com/wiki/1252599548343744
---
+++ liaoxuefeng-java-learning/ch1/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    /**
+     * my first java program
+     * 
+     * @author yangshixi
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
